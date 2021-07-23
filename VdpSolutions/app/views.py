a45a9c13f2cfd33e4a5f1ee1f638a193d2b8c8b9
Manthan G N <manthangn2000@gmail.com>
2021-07-23 22:39:34 +0530

    first commit

---
+++ VdpSolutions/app/views.py
@@ -0,0 +1,18 @@
+from app.models import Contact
+from django.shortcuts import render
+from .models import Contact
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        contact.name=name
+        contact.email=email
+        contact.subject=subject
+        contact.save()
+        return HttpResponse("<h1>THANKS FOR CONTACT US</h2>")
+    return render(request,'index.html')
